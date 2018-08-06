eded356ec7b97c1996a19bc73b545393d5effe97
CSC <sicheng906531182@qq.com>
2018-08-06 22:44:48 +0800

    add Main.java

--- windows/src/application/Main.java
+++ windows/src/application/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			BorderPane root = new BorderPane();		
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setScene(scene);
 			primaryStage.show();
