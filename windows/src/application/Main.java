e98cf521544ac992b7f72a2a544459e74311228a
CSC <sicheng906531182@qq.com>
2018-09-16 13:18:40 +0800

    目录树对接

--- windows/src/application/Main.java
+++ windows/src/application/Main.java
@@ -2,10 +2,13 @@ package application;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import model.disk.DiskFileTree;
 import model.disk.FAT;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import view.disk.DiskFileTreePane;
 import view.ui.RootPane;
 
 public class Main extends Application {
@@ -14,18 +17,22 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 
-		Main.primaryStage = primaryStage;
-		FAT.getInstance().toString();
-		try {
-			primaryStage.initStyle(StageStyle.UNDECORATED);
-			primaryStage.setMaximized(true);
-			RootPane root = new RootPane(primaryStage);
-			Scene scene = new Scene(root, primaryStage.getWidth(), primaryStage.getHeight());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		Scene scene = new Scene(DiskFileTreePane.getInstance());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+//		Main.primaryStage = primaryStage;
+//		FAT.getInstance().toString();
+//		try {
+//			primaryStage.initStyle(StageStyle.UNDECORATED);
+//			primaryStage.setMaximized(true);
+//			RootPane root = new RootPane(primaryStage);
+//			Scene scene = new Scene(root, primaryStage.getWidth(), primaryStage.getHeight());
+//			primaryStage.setScene(scene);
+//			primaryStage.show();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 
 	public static void main(String[] args) {
