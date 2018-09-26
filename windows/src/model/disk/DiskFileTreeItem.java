4d425bad20ee74535761ede03c7b444385522807
CSC <sicheng906531182@qq.com>
2018-09-26 19:32:44 +0800

    修改目录树图标

--- windows/src/model/disk/DiskFileTreeItem.java
+++ windows/src/model/disk/DiskFileTreeItem.java
@@ -1,14 +1,13 @@
 package model.disk;
 
-import javafx.scene.Node;
 import javafx.scene.control.TreeItem;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class DiskFileTreeItem extends TreeItem<FileItem>{
-	private final Node directoryIcon = new ImageView(
-			new Image(getClass().getResourceAsStream("../../images/directory.png")));
-	private final Node filesIcon = new ImageView(new Image(getClass().getResourceAsStream("../../images/files.png")));
+	private final ImageView directoryIcon = new ImageView(
+			new Image(getClass().getResourceAsStream("../../images/folder.png"), 30, 30, true, true));
+	private final ImageView filesIcon = new ImageView(new Image(getClass().getResourceAsStream("../../images/txt.png"), 30, 30, true, true));
 	
 	public DiskFileTreeItem(FileItem fileItem) {
 		super(fileItem);
